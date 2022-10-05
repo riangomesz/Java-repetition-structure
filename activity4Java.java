8f25b29fbda7fcdc9ae797f9f67d8d14701503a0
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-10-05 15:08:41 -0300

    Add files via upload

---
+++ activity4Java.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		int numero = 1;
+		int valor;
+		
+		System.out.println("Informe um Valor Inteiro: ");
+		valor = leitor.nextInt();
+		System.out.println("Os Números Ímpares de 1 até "+valor+" são: ");
+		
+		while(numero<=valor){
+		    if(numero%2!=0){
+		        System.out.println(numero);
+		    }
+		    numero = numero+ 1;
+		}
+	}
+}
